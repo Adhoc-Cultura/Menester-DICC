2588b946b1d762808e07c214fc2878ce99cbd1d6
Adhoc Cultura <oscar@adhoc-cultura.com>
2026-08-27 19:23:17 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MENESTER from "./MENESTER.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MENESTER />
-  </React.StrictMode>
-);
